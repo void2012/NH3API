3c1247df1dc61d73e5fdf6af5d046c60cfcb302c
void_17 <heroerror3@gmail.com>
2025-10-04 20:13:04 +0700

    Update file headers
    Due to my nickname change

--- nh3api/core/nh3api_std/algorithm.hpp
+++ nh3api/core/nh3api_std/algorithm.hpp
@@ -1,7 +1,7 @@
 //===----------------------------------------------------------------------===//
 //
 // Part of the NH3API, under the Apache License v2.0.
-// Copyright void_17, 2024-2025
+// Copyright (C) devoider17 (aka void_17), 2024-2025
 // You may use this file freely as long as you list the author and the license
 // In the source code files of your project
 // SPDX-License-Identifier: Apache-2.0
